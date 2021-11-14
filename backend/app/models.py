718455edc4b805fa9132cf747d00150a74ef1a6c
Jake Capra <jacob.capra@mymail.champlain.edu>
2021-11-14 17:46:42 -0500

    got some stuff in for the API, need to reorganize and clean it up

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,19 @@
+from flask import json
+from flask.globals import current_app
+from flask.helpers import url_for
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from app.exceptions import ValidationError
 from app import db
 from enum import unique
 from datetime import datetime
+
+
+class Permission:
+    FOLLOW = 1
+    COMMENT = 2
+    WRITE = 4
+    MODERATE = 8
+    ADMIN = 16
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -9,6 +22,26 @@ class User(db.Model):
     last_name = db.Column(db.String(30), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id}).decode('utf-8')
+    
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+    
+    def to_json(self):
+        json_user = {
+            'url': url_for('app.get_user', id=self.id),
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+        }
+        return json_user
     
     # for debug purposes
     def __repr__(self):
@@ -17,9 +50,27 @@ class User(db.Model):
     
 class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     text = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
-        # for debug purposes
+    def to_json(self):
+        json_review = {
+            'url': url_for('app.get_review', id=self.id),
+            'id': self.id,
+            'text': self.text,
+            'created_at': self.created_at
+        }
+        return json_review
+    
+    @staticmethod
+    def from_json(json_review):
+        text = json_review.get('body')
+        if text is None or text =='':
+            raise ValidationError('review does not have text')
+        return Review(text=text)
+    
+    
+    # for debug purposes
     def __repr__(self):
         return '<Review {}>'.format(self.id)
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,41 @@
-from app import app
+from flask import json, request, g, jsonify, url_for
+from app import app, db
+from models import User, Review, Permission
+from .decorators import permission_required
+from .errors import forbidden
 
 @app.route('/')
 def index():
     return "Hello World"
+
+@app.route('/reviews/')
+def get_reviews():
+    reviews = Review.query.all()
+    return jsonify({ 'reviews': [review.to_json() for review in reviews] })
+
+@app.route('/reviews/<int:id>')
+def get_review(id):
+    review = Review.query.get_or_404(id)
+    return jsonify(review.to_json())
+
+@app.route('/reviews', methods=['POST'])
+@permission_required(Permission.WRITE)
+def new_post():
+    review = Review.from_json(request.json)
+    review.author_id = g.current_user
+    db.session.add(review)
+    db.session.commit()
+    return jsonify(review.to_json()), 201, \
+        {'Location': url_for('api.get_post', id=review.id)}
+        
+@app.route('/reviews/<int:id>', methods=['PUT'])
+@permission_required(Permission.WRITE)
+def edit_post(id):
+    review = Review.query.get_or_404(id)
+    if g.current_user != review.author_id and \
+            not g.current_user.can(Permission.ADMIN):
+        return forbidden('Insufficient permissions')
+    review.text = request.json.get('text', review.text)
+    db.session.add(review)
+    db.session.commit()
+    return jsonify(review.to_json())
