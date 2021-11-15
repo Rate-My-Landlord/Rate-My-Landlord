80a639d3da45ecee4ddc701a00e3a6ededb5f227
Hayden-Stegman <hayden.stegman@gmail.com>
2021-11-15 16:51:37 -0500

    Orginally front end was initialized with a template. This was to hopefully lay a foundation starting point for our application. After looking thru the template, its not needed. Simply removed the front end and re-initialized it as an empty project this time.

--- Frontend-ReactNative/App.tsx
+++ Frontend-ReactNative/App.tsx
@@ -1,23 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
-  }
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>It works?</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
