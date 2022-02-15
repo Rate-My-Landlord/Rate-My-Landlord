c5fc7cdfdbf21e88b700beb4c3bdbd4a03df4b3c
Hayden-Stegman <hayden.stegman@gmail.com>
2022-02-15 14:55:43 -0500

    Layout Framework finnished on par with Wireframes for both IOS and Web! Functionality Implementation is ready.

--- Frontend-RateMyLandlord/screens/HomeScreen.tsx
+++ Frontend-RateMyLandlord/screens/HomeScreen.tsx
@@ -17,14 +17,22 @@ const HomeScreen = () => {
     <>
       <View style={styles(windowWidth).headerScreen}><Text style={styles(windowWidth).textColor}>HEADER</Text></View>
       <View style={styles(windowWidth).bodyScreen}>
+        
+        {/* Main Content Container */}
         <View style={styles(windowWidth).mainContainer}>
-          <Text style={styles(windowWidth).textColor}>CENTER</Text>
+          <View style={styles(windowWidth).listContainer}>
+            <Text style={styles(windowWidth).textColor}>List Container</Text>
+          </View>
+
+          <View style={styles(windowWidth).listControlContainer}>
+            <Text style={styles(windowWidth).textColor}>List Controler</Text>
+          </View>
         </View>
         
-        { // Right Container Only on Web
-          Platform.OS !== 'ios' && Platform.OS !== 'android' ? (
+        { // Right Container Only on Web and when screen is big
+          (Platform.OS !== 'ios' && Platform.OS !== 'android') && windowWidth >= screenChangePoint ? (
             <View style={styles(windowWidth).rightContainer}>
-              <Text style={styles(windowWidth).textColor}>RIGHT</Text>
+              <Text style={styles(windowWidth).textColor}>Ad Space?</Text>
             </View>
           ):(<></>)
         }
@@ -43,27 +51,49 @@ const styles = (windowWidth : any) => StyleSheet.create({
     backgroundColor: '#1f2937',
     justifyContent: 'center',
     alignItems: 'center',
-    paddingTop: Platform.OS === 'ios' || Platform.OS === 'android' ? 30 : 0,
+    paddingTop: Platform.OS === 'ios' || Platform.OS === 'android' ? 40 : 0,
   },
   bodyScreen: {
     flex: 10,
-    flexDirection: windowWidth >= screenChangePoint ? "row" : "column",
-    padding: Platform.OS === 'ios' || Platform.OS === 'android' ? 5 : 20,
+    flexDirection: windowWidth >= screenChangePoint ? "row" : "column-reverse",
+    paddingTop: Platform.OS === 'ios' || Platform.OS === 'android' ? 10 : 15,
+    paddingHorizontal: (Platform.OS === 'ios' || Platform.OS === 'android') ? 0 : '10%',
     backgroundColor: '#ffffff',
   },
   
   // Body Containers
   rightContainer: {
     flex: 1,
+    marginHorizontal: 10,
+    
+    //Temp for Visibility
     backgroundColor: 'red',
-    margin: 10,
+
+    // Temp for Text Viewing
     justifyContent: 'center',
     alignItems: 'center',
   },
   mainContainer: {
-    backgroundColor: '#9ca3af',
-    flex: windowWidth >= screenChangePoint ? 2 : 6,
-    margin: 10,
+
+    // Change FlexBox valuse based on screen size
+    marginHorizontal: 10,
+    
+    // Flex Settings
+    flex: 3,
+    flexDirection: windowWidth >= screenChangePoint ? 'row-reverse' : 'column-reverse',
+  },
+
+  // Content Containers
+  listContainer: {
+    flex: windowWidth >= screenChangePoint ? 2 : 5,
+    backgroundColor: "green",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  // Contains Filter box, buttons, ect.
+  listControlContainer: {
+    flex: 1,
+    backgroundColor: "orange",
     justifyContent: 'center',
     alignItems: 'center',
   },
