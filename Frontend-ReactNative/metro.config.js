ee2101709bbf1fd61eabb4348ac0e14f0e5bcc32
Jake Capra <jacob.capra@mymail.champlain.edu>
2022-02-21 12:06:07 -0500

    fixed ios issue with merto and expo

--- Frontend-ReactNative/metro.config.js
+++ Frontend-ReactNative/metro.config.js
@@ -1,9 +1,9 @@
-// Hayden Stegman
-// Adds the 'cjs' extension to Expo.
 const { getDefaultConfig } = require('@expo/metro-config');
 
-const defaultConfig = getDefaultConfig(__dirname);
+const config = getDefaultConfig(__dirname);
 
-defaultConfig.resolver.assetExts.push('cjs');
+config.resolver.sourceExts = process.env.RN_SRC_EXT
+    ? [...process.env.RN_SRC_EXT.split(',').concat(config.resolver.sourceExts), 'cjs'] // <-- cjs added here
+    : [...config.resolver.sourceExts, 'cjs'] // <-- cjs added here
 
-module.exports = defaultConfig;
+module.exports = config;
